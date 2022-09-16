0d40280bcc878e18278a7ec43281dce6a337d174
unknown <simkevicius.viktoras@gmail.com>
2022-09-16 20:45:59 +0300

    instructions added

--- src/pages/teams.js
+++ src/pages/teams.js
@@ -5,20 +5,29 @@ export default function Teams(props){
     return(
       <div className="teams-main">
         <div className="teams-main-container">
-        <h1 className="teams-heading" >Generated teams:</h1>
-        <div className="generated-teams">
-          {props.displaySavedTeams}       
-          <div>
-          <h2 className="team-number">Generated team</h2>
-            {props.generatedTeams}
-          </div> 
+          <h1 className="teams-heading" >Generated teams:</h1>        
+          <div className="generated-teams"> 
+            <div className="generated-teams-inner">
+              {props.displaySavedTeams}       
+              <div>
+                <h2 className="team-number">Generated team</h2>
+                {props.generatedTeams}
+              </div> 
+            </div>  
+            
+          </div>
+          <div className="buttons">
+            <button type="button" className="reset-btn" onClick={props.reset} >Reset and change players</button>
+            <button type="button" className="save-team-btn" onClick={()=>{props.saveTeam();}} >Confirm Teams</button>
+            <button type="button" className="generate-btn" onClick={props.generateNextTeams} >Generate Next Teams</button>
+          </div>  
+          <div className="instructions">
+            <h2 className="instruction-heading">How to use the App:</h2>  
+            <p>Confirm Teams button - saves generated teams in memory.</p>
+            <p>Generate Next Teams button – generates teams but not the same as saved teams.</p>
+            <p>Reset and change players button – erases saved teams memory and allows to change players.</p>
+          </div>      
         </div>
-        <div className="buttons">
-          <button type="button" className="reset-btn" onClick={props.reset} >Reset and change players</button>
-          <button type="button" className="save-team-btn" onClick={()=>{props.saveTeam();}} >Confirm Teams</button>
-          <button type="button" className="generate-btn" onClick={props.generateNextTeams} >Generate Next Teams</button>
-        </div>        
-      </div>
       </div>
       
     )
